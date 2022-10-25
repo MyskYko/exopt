020adddf05190fdf998ed8d08c3190b793df6fce
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-25 02:39:01 -0700

    clean printing

--- src/main.cpp
+++ src/main.cpp
@@ -16,20 +16,25 @@
 
 using namespace std;
 
-void rem(aigman &aig, vector<int> &gates) {
-  aig.vValues.clear();
-  aig.vValues.resize(aig.nObjs);
-  for(int i: gates) {
-    for(int ii = i + i; ii <= i + i + 1; ii++) {
-      aig.vValues[aig.vObjs[ii] >> 1]++;
-    }
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> const &v) {
+  string delim;
+  for(T const &i: v) {
+    os << delim << i;
+    delim = ", ";
   }
-  for(auto it = gates.begin(); it != gates.end();) {
-    if((int)aig.vvFanouts[*it].size() == aig.vValues[*it]) {
-      it = gates.erase(it);
-      continue;
-    }
-    it++;
+  return os;
+}
+
+ostream &operator<<(ostream &os, Cut const &cut) {
+  os << "{" << cut.leaves << "}";
+  return os;
+}
+
+template <typename T>
+void PrintVecWithIndex(vector<T> const &v, string prefix = "") {
+  for(int i = 0; i < (int)v.size(); i++) {
+    cout << prefix << i << " : " << v[i] << endl;
   }
 }
 
@@ -40,26 +45,13 @@ int main(int argc, char **argv) {
   string aigname = argv[1];
   aigman aig(aigname);
   aig.supportfanouts();
-  string delim;
   bool fSynthesized = true;
   while(fSynthesized) {
     fSynthesized = false;
     // cut enumeration
     vector<vector<Cut> > cuts;
     CutEnumeration(aig, cuts);
-    /*
-    for(int i = 0; i < aig.nObjs; i++) {
-      for(auto &cut: cuts[i]) {
-        cout << i << " : ";
-        delim = "";
-        for(int j: cut.leaves) {
-          cout << delim << j;
-          delim = ", ";
-        }
-        cout << endl;
-      }
-    }
-    */
+    //PrintVecWithIndex(cuts);
     // cut to gates
     map<vector<int>, vector<int> > mGates;
     for(int i = 0; i < aig.nObjs; i++) {
@@ -109,46 +101,14 @@ int main(int argc, char **argv) {
       auto const &outputs = get<1>(p.second);
       const bool fReach = get<2>(p.second);
       int nGates = gates.size();
-      delim = "";
-      for(int j: inputs) {
-        cout << delim << j;
-        delim = ", ";
-      }
-      cout << " : ";
-      delim = "";
-      for(int j: gates) {
-        cout << delim << j;
-        delim = ", ";
-      }
-      cout << endl;
-      delim = "";
-      for(int j: inputs) {
-        cout << delim << j;
-        delim = ", ";
-      }
-      cout << " : ";
-      delim = "";
-      for(int j: outputs) {
-        cout << delim << j;
-        delim = ", ";
-      }
-      cout << endl;
+      cout << inputs << " : " << gates << endl;
+      cout << inputs << " : " << outputs << endl;
       // single window mode
       if(!fReach && nGates <= 7) {
         // get relation
         vector<vector<bool> > br;
         GetBooleanRelation(aig, inputs, outputs, br);
-        /*
-        for(int i = 0; i < (int)br.size(); i++) {
-          cout << i << " : ";
-          delim = "";
-          for(int j = 0; j < (int)br[i].size(); j++) {
-            cout << delim << br[i][j];
-            delim = ", ";
-          }
-          cout << endl;
-        }
-        */
+        //PrintVecWithIndex(br);
         // synthesis
         cout << "Optimize a cut of " << nGates << " gates" << endl;
         ExMan<KissatSolver> exman(br);
@@ -194,27 +154,16 @@ int main(int argc, char **argv) {
           continue;
         }
         for(auto const &cut: cuts[i]) {
-          if(cut.leaves == inputs || cut.leaves.size() == 1) {
+          auto const &inputs2 = cut.leaves;
+          if(inputs2 == inputs || inputs2.size() == 1) {
             continue;
           }
-          auto const &p2 = mWindows[cut.leaves];
+          auto const &p2 = mWindows[inputs2];
           auto const &gates2 = get<0>(p2);
           auto const &outputs2 = get<1>(p2);
           const bool fReach2 = get<2>(p2);
           int nGates2 = gates2.size();
-          cout << "\t\t";
-          delim = "";
-          for(int j: cut.leaves) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << " : ";
-          delim = "";
-          for(int j: gates2) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << endl;
+          cout << "\t\t" << inputs2 << " : " <<  gates2 << endl;
           if(nGates2 > 7) {
             cout << "\t\tSkipped because the cone has more than 7 gates" << endl;
             continue;
@@ -223,19 +172,7 @@ int main(int argc, char **argv) {
             cout << "\t\tSkipped because the cone is not included" << endl;
             continue;
           }
-          cout << "\t\t";
-          delim = "";
-          for(int j: cut.leaves) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << " : ";
-          delim = "";
-          for(int j: outputs2) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << endl;
+          cout << "\t\t" << inputs2 << " : " <<  outputs2 << endl;
           // should be switched
           if(fReach2 || aig.reach(outputs2, inputs)) {
             cout << "\t\tSkipped due to potential loops" << endl;
@@ -243,13 +180,7 @@ int main(int argc, char **argv) {
           }
           vector<int> gates_(nGates);
           gates_.resize(set_difference(gates.begin(), gates.end(), gates2.begin(), gates2.end(), gates_.begin()) - gates_.begin());
-          cout << "\t\tGates outside of the cone : ";
-          delim = "";
-          for(int j: gates_) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << endl;
+          cout << "\t\tGates outside of the cone : " << gates_ << endl;
           for(auto it = gates_.begin(); it != gates_.end();) {
             if(aig.reach(outputs2, vector<int>{*it})) {
               it = gates_.erase(it);
@@ -257,37 +188,13 @@ int main(int argc, char **argv) {
             }
             it++;
           }
-          cout << "\t\tExtra inputs : ";
-          delim = "";
-          for(int j: gates_) {
-            cout << delim << j;
-            delim = ", ";
-          }
-          cout << endl;
+          cout << "\t\tExtra inputs : " << gates_ << endl;
           vector<vector<bool> > br;
           GetBooleanRelation(aig, inputs, outputs2, br);
-          /*
-          for(int k = 0; k < (int)br.size(); k++) {
-            cout << "\t\t" << k << " : ";
-            delim = "";
-            for(int j = 0; j < (int)br[k].size(); j++) {
-              cout << delim << br[k][j];
-              delim = ", ";
-            }
-            cout << endl;
-          }
-          */
+          //PrintVecWithIndex(br, "\t\t");
           vector<vector<bool> > sim;
           GetSim(aig, inputs, gates_, sim);
-          /*
-          for(int k = 0; k < (int)sim.size(); k++) {
-            cout << "\t\t" << k << " : ";
-            for(int j = 0; j < (int)sim[k].size(); j++) {
-              cout << sim[k][j];
-            }
-            cout << endl;
-          }
-          */
+          //PrintVecWithIndex(sim, "\t\t");
           cout << "\t\tOptimize a cut of " << nGates2 << " gates" << endl;
           ExMan<KissatSolver> exman(br, &sim);
           aigman *aig2;
